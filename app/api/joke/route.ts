a41538523713e230138d3f51d97c981f455b5f11
Abhishek kumar <abhishekgupta9640@gmail.com>
2025-12-10 16:22:30 +0530

    commit

--- app/api/joke/route.ts
+++ app/api/joke/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+    const response = await fetch('https://official-joke-api.appspot.com/random_joke',
+         { next: { revalidate: 10 } }
+    );
     const data = await response.json();
     return NextResponse.json(data);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,13 @@
-'use client';
+export default async function JokePage() {
+  const joke = await fetch('http://localhost:3000/api/joke', {
+    cache: 'no-store'
+  }).then(res => res.json());
 
-import { useEffect, useState } from 'react';
-
-export default function JokePage() {
-  const [joke, setJoke] = useState<{ setup: string; punchline: string } | null>(null);
-
-  useEffect(() => {
-    fetch('/api/joke')
-      .then(res => res.json())
-      .then(data => setJoke(data));
-  }, []);
-
-  if (!joke) return <p>Loading...</p>;
-
-return (
-  <div className="p-4">
-    <h2 className="text-xl font-semibold mb-2">Random Joke</h2>
-    <p className="text-white">{joke.setup}</p>
-    <p className="font-medium mt-1">{joke.punchline}</p>
-  </div>
- );
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-2">Random Joke</h2>
+      <p className="text-white">{joke.setup}</p>
+      <p className="font-medium mt-1">{joke.punchline}</p>
+    </div>
+  );
 }
